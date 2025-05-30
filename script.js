3141a615813aa64c84065553024ed4e0df7642fc
deniscelant <101429050+deniscelant@users.noreply.github.com>
2025-05-29 23:05:54 -0300

    payment change

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ const plates = [
   {
     id: 1,
     nome: "Salada",
-    img: ".salada.jpg",
+    img: "img/salada.jpg",
     price: 20,
   },
   {
     id: 2,
     nome: "Macarrão",
-    img: ".macarrao.jpg",
+    img: "img/macarrao.jpeg",
     price: 25,
   },
   {
     id: 3,
     nome: "Feijoada",
-    img: ".\feijoada.jpg",
+    img: "img/feijoada.jpg",
     price: 40,
   },
   {
     id: 4,
     nome: "Frango grelhado",
-    img: ".\frango.jpg",
+    img: "img/frango.jpg",
     price: 16,
   },
 ];
@@ -28,45 +28,60 @@ const plates = [
 let plateIndex;
 const descount = false;
 
-const params = new URLSearchParams(window.location.search);
+if (document.title === "Checkout Restaurante") {
+  const params = new URLSearchParams(window.location.search);
 
-const nome = params.get("nome");
-const veg = params.get("veg");
-const diet = params.get("diet");
+  const nome = params.get("nome");
+  const veg = params.get("veg");
+  const diet = params.get("diet");
 
-document.querySelector("#welcome").textContent =
-  "Olá " + nome + "! veja os detalhes do seu pedido:";
+  init();
 
-function choosePlate() {
-  if (veg == "Sim" && diet == "Sim") {
-    return (plateIndex = 1);
-  }
-  if (veg == "Sim" && diet == "Não") {
-    return (plateIndex = 2);
-  }
-  if (veg == "Não" && diet == "Sim") {
-    return (plateIndex = 4);
-  }
-  if (veg == "Não" && diet == "Não") {
-    return (plateIndex = 3);
-  }
-}
+  function init() {
+    document.querySelector("#welcome").textContent =
+      "Olá " + nome + "! veja os detalhes do seu pedido:";
 
-function checkout() {
-  document.querySelector("#plate").innerHTML = `
-    <h3>${plates[plateIndex].nome}</h3>
-    <img>${plates[plateIndex].img}</img>
-    <h2 id="platePrice">${plates[plateIndex].price}</h2>
-    `;
-}
-
-function handleDescount() {
-  const payment = document.querySelector("#payment");
-  if (payment.selectedIndex === "Dinheiro") {
-    descount = true;
-  } else {
-    descount = false;
+    choosePlate();
+    checkout();
   }
-  const descountPrice = platePrice - 15;
-  const platePrice = document.querySelector("#platePrice").textContent;
+
+  function choosePlate() {
+    if (veg === "Sim" && diet === "Sim") {
+      return (plateIndex = 0);
+    } else if (veg === "Sim" && diet === "Não") {
+      return (plateIndex = 1);
+    } else if (veg === "Não" && diet === "Sim") {
+      return (plateIndex = 3);
+    } else if (veg === "Não" && diet === "Não") {
+      return (plateIndex = 2);
+    }
+  }
+
+  function checkout() {
+    document.querySelector("#plate").innerHTML = `
+      <h3>${plates[plateIndex].nome}</h3>
+      <img src=${plates[plateIndex].img}></img>
+      <h2 id="platePrice">R$${plates[plateIndex].price},00</h2>
+      `;
+    const payment = document.querySelector("#payment");
+    function descountPrice(){
+      const platePrice = document.querySelector("#platePrice");
+      // const numberPlatePrice = Number(platePrice.innerText)
+      // const newPlatePrice = numberPlatePrice - 15;
+      // platePrice.innerText = newPlatePrice;
+      const number = Number(platePrice.textContent)
+      platePrice.textContent = number + 1
+    }
+
+    payment.onchange = () => {
+      // console.log(payment.selectedIndex)
+      if (payment.selectedIndex == 1) {
+        descountPrice()
+      } else {
+        return
+      }
+    };
+  }
+
+ 
 }
